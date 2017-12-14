fad889f21a3698486ce668c29233d9d078fb92ca
Alex Soudant <soudant.alex@gmail.com>
2017-12-14 13:55:48 +0100

    adding fact provider

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { FactsProvider } from './../../providers/facts/facts';
+import { Observable } from 'rxjs/Observable';
+import { Facts } from '../../interfaces/facts.interface';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,13 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
-  }
+  RandomFacts: Observable<Facts[]>;
+  
+    constructor(public factsProvider: FactsProvider) {
+  
+      this.RandomFacts = factsProvider.getRandom();
+  
+    }
+  
 
 }
