90c7945ca55e927de2458b6a74e22e69ec7ee959
etienneYnov <etienne.hounguevou@ynov.com>
2017-12-21 12:30:51 +0100

    solving merge

--- src/pages/moviedetails/moviedetails.ts
+++ src/pages/moviedetails/moviedetails.ts
@@ -59,13 +59,13 @@ export class MoviedetailsPage {
         this.film = res;
         console.log(this.film);
         this.peoples = this.peopleProvider.getPeoplesById(this.film.characters)
-        .map(peoples => peoples.map(people=>this.addImg(people)));
+          .map(peoples => peoples.map(people => this.addImg(people)));
         this.planets = this.planetProvider.getPlanetsById(this.film.planets)
-        .map(planets => planets.map(planet=>this.addImg(planet)));
+          .map(planets => planets.map(planet => this.addImg(planet)));
         this.starships = this.starshipsProvider.getStarshipsById(this.film.starships)
-        .map(starships => starships.map(starship=>this.addImg(starship)));
+          .map(starships => starships.map(starship => this.addImg(starship)));
         this.vehicles = this.vehiclesProvider.getVehiclesById(this.film.vehicles)
-        .map(vehicles => vehicles.map(vehicle=>this.addImg(vehicle)));
+          .map(vehicles => vehicles.map(vehicle => this.addImg(vehicle)));
 
       }),
       ((error) => {
@@ -79,19 +79,18 @@ export class MoviedetailsPage {
 
   }
 
-<<<<<<< HEAD
+
   goToItemDetailsPage(id: string) {
     this.navCtrl.push(ItemDetailsPage, {
       peopleId: id
     });
   }
-=======
+
   addImg(element: any): any {
-    element.img = element.name.replace(/ /g,'_') + ".jpg"
+    element.img = element.name.replace(/ /g, '_') + ".jpg"
     return element
-    
+
   }
 
 
->>>>>>> 0a0b207b24497afe5bf8db84d14983abab12fbdc
 }
